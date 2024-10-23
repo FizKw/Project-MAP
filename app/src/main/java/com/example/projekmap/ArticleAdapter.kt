e9f39cceae499e7c2915e5ddcfd5e75f3dc940f3
Vincent Marlino <71843843+Marlino69@users.noreply.github.com>
2024-10-24 04:28:50 +0700

    fix: fixing the routing to article fragment

--- app/src/main/java/com/example/projekmap/ArticleAdapter.kt
+++ app/src/main/java/com/example/projekmap/ArticleAdapter.kt
@@ -8,7 +8,6 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.google.ai.client.generativeai.type.content
 
 data class Article(
     val title: String,  // Judul artikel
